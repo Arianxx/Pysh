b5539a37109c61899ec0a5c39e74ae303dadaefb
Arianxx <34032486+Arianxx@users.noreply.github.com>
2018-06-05 23:47:21 +0800

    自动补全

--- pysh/manage/env.py
+++ pysh/manage/env.py
@@ -16,15 +16,15 @@ class Application:
         if not getattr(cls, '_instance', None):
             cls._instance = super().__new__(cls, *args, **kwargs)
 
-        try:
-            import readline
-            from .middleware import Completer
-        except ImportError:
-            # Win下没有readline模块
-            pass
-        else:
-            readline.parse_and_bind("tab: complete")
-            readline.set_completer(Completer.search_symbol)
+            try:
+                import readline
+                from .middleware import Completer
+            except ImportError:
+                # Win下没有readline模块
+                pass
+            else:
+                readline.parse_and_bind("tab: complete")
+                readline.set_completer(Completer.search_symbol)
 
         return cls._instance
 
